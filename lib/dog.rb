7445f62cf6ca4343ea46262eeab8112e09c800be
Jack Goode <jack.w.goode@icloud.com>
2020-06-16 11:56:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,7 +7,7 @@ class Dog
   
   def initialize(name)
     self.name = name
-    @@all << self
+    self.save
   end
   
   def self.all
@@ -18,6 +18,10 @@ class Dog
     @@all.each do |dog|
     puts dog.name
     end 
+  end
+  
+  def self.save
+    @@all << self
   end
   
   
